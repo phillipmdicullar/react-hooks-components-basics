7ac400b049e0e4c7a6d7ccdfeb8c79400ef4611e
Philip <phillip.emdokolo@student.moringaschool.com>
2024-08-01 11:30:26 +0300

    Just understood how to export and import perfectly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Mass from "./__tests__/Mass";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Mass />
     </div>
   );
 }
---
+++ src/__tests__/Mass.js
@@ -0,0 +1,3 @@
+import React from "react"
+const Mass = () => <h1>This is a cyber threat intelligence</h1>
+export default Mass;
